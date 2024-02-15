d79190db9c0b59dd3b44da41181a91cb20f087b8
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2024-02-15 12:54:09 +0100

    Update Serika.py

--- Serika.py
+++ Serika.py
@@ -1,7 +1,6 @@
 import discord
 import os
-from datetime import datetime
-from vertexai.preview.generative_models import GenerativeModel, HarmBlockThreshold, HarmCategory
+from vertexai.preview.generative_models import GenerativeModel
 
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
@@ -11,42 +10,27 @@ intents.messages = True
 class MyBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, intents=intents)
-        self.model = GenerativeModel("gemini-pro-vision")
+        self.model = GenerativeModel("gemini-pro")
         self.sessions = {}
 
     async def on_ready(self):
         print(f'Logged in as {self.user}')
 
-    def load_prompt(self):
-        with open('prompt.txt', 'r', encoding='utf-8') as file:
-            return file.read().strip()
-
-    async def generate_response(self, session_id, prompt):
+    async def ensure_chat_session(self, session_id):
         if session_id not in self.sessions:
             self.sessions[session_id] = self.model.start_chat()
-
-        session = self.sessions[session_id]
-
-        response = session.generate_response(prompt)
-        response_text = response.text
-        
-
-        return response_text
+        return self.sessions[session_id]
 
     async def on_message(self, message):
         if message.author == self.user:
             return
 
+        session_id = message.channel.id  # Use channel ID for session management
+        chat = await self.ensure_chat_session(session_id)
+
         async with message.channel.typing():
-            base_prompt = self.load_prompt()
-            session_id = message.channel.id
-            
-            try:
-                response_message = await self.generate_response(session_id, base_prompt)
-                await message.channel.send(response_message)
-            except Exception as e:
-                print(f"Error: {e}")
-                await message.channel.send("Sorry, I can't respond at the moment.")
+            response = chat.send_message(message.content)
+            await message.channel.send(response.text)
 
 bot = MyBot()
 bot.run(DISCORD_BOT_TOKEN)
