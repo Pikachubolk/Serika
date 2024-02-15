8c042a47a90f0907ed34bce7444e9b1f525ced6c
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2024-02-16 00:26:41 +0100

    Update Serika.py

--- Serika.py
+++ Serika.py
@@ -28,7 +28,7 @@ safety_settings = {
 class MyBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, intents=intents)
-        self.model = GenerativeModel("gemini-pro-vision")
+        self.model = GenerativeModel("gemini-1.0-pro-vision")
         self.sessions = {}
         self.storage_client = storage.Client()
         self.bucket_name = 'serika-images'
